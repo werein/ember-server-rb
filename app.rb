13e88205feaae058a16ddc092dff1ab179afa676
Jiri Kolarik <jiri.kolarik@wereinhq.com>
2015-02-14 18:55:26 +0100

    Process everything to ember

--- app.rb
+++ app.rb
@@ -4,13 +4,13 @@ require "redis"
 
 class App < Sinatra::Base
 
-  def index(key)  
+  def index key
     redis = Redis.new url: ENV['REDIS_URL']
     key ||= redis.get "#{ENV['APP']}:current"
     redis.get key
   end
 
-  get '/' do  
+  get '*' do  
     content_type 'text/html'
     index params[:key]
   end  
